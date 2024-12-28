57257db5babd336ea610c26e0ebcb527eba34f21
Nathan Bills <nathan@nerdmeeting.com>
2024-12-28 16:11:24 -0700

    Use a splat `*` expression to output the private IP addresses of all aws_instances.
    The splat expression captures all objects in a list that share an attribute. The special * symbol iterates over all the elements of a given list and returns information based on the shared attribute you define.
    Without the splat expression, Terraform would not be able to output the entire array of your instances and would only return the first item in the array.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "tags" {
   description = "Instance tags"
   value       = aws_instance.ubuntu.tags
 }
+
+output "private_addresses" {
+  description = "Private DNS for AWS instances"
+  value       = aws_instance.ubuntu[*].private_dns
+}
